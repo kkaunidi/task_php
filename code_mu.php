0dae716e683aee41ae458d5adb60a282084f8517
kkaunidi <129606927+kkaunidi@users.noreply.github.com>
2024-05-31 21:29:55 +0300

    3105-2

--- code_mu.php
+++ code_mu.php
@@ -1088,3 +1088,50 @@ for($i = 0; $i < $length; $i++) {
 }
 var_dump($arr);
 ?> -->
+<!-- 146 Практика на массивы в цикле в PHP
+
+Дан следующий массив с работниками и их зарплатами:
+Увеличьте зарплату каждого работника на 10%. -->
+
+	<!--<?php
+	 // $arr = [
+	// 	'employee1' => 100,
+	// 	'employee2' => 200,
+	// 	'employee3' => 300,
+	// 	'employee4' => 400,
+	// 	'employee5' => 500,
+	// 	'employee6' => 600,
+	// 	'employee7' => 700,
+	// ];
+	
+	// foreach ($arr as $elem){
+	// 	$elem = $elem  + $elem*0.1;
+	// 	echo'<pre>';
+	// 	print_r($elem);  
+	// 	echo'</pre>';
+	// }
+	// echo'<pre>';
+	//   print_r($elem);  
+	//   echo'</pre>'; -->
+?>
+<!-- 
+	Модифицируйте предыдущую задачу так, чтобы зарплата увеличивалась только тем работникам, у которых она меньше или равна 400. -->
+	<!-- <?php
+	$arr = [
+		'employee1' => 100,
+		'employee2' => 200,
+		'employee3' => 300,
+		'employee4' => 400,
+		'employee5' => 500,
+		'employee6' => 600,
+		'employee7' => 700,
+	];
+	foreach($arr as $elem){
+		if($elem <= 400){
+			$elem = $elem  + $elem*0.1;
+		}
+		echo'<pre>';
+	 	print_r($elem);  
+	 	echo'</pre>';
+	}
+	?> -->
